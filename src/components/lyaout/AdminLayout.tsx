abac9cc3cc27356b287b331415639916e5837577
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-15 00:32:27 +0600

    reusable accordion and modal added

--- src/components/lyaout/AdminLayout.tsx
+++ src/components/lyaout/AdminLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Button from "../Button";
 import Modal from "../ui/Modal";
+import { Accordion, AccordionItem } from "../ui/Accordian";
 
 const AdminLayout = () => {
   const [modal,setModal]=useState(false)
@@ -23,7 +24,143 @@ const AdminLayout = () => {
       >
         Click Meh
       </Button>
-      <Modal modal={modal} setModal={setModal}></Modal>
+      <Modal modal={modal} setModal={setModal}>
+      
+        <div className="">
+        <Accordion >
+          <AccordionItem title="Why you are so beautiful?">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit cumque neque tempora aspernatur ratione ullam ipsum
+              vero impedit quibusdam commodi optio accusamus iusto praesentium
+              dicta ipsa blanditiis laudantium omnis illum pariatur labore,
+              repellendus provident quo sapiente inventore? Placeat sint,
+              mollitia fugit excepturi quae ad recusandae ullam quo consequuntur
+              illo quos labore sit fugiat obcaecati a provident nam repudiandae
+              maiores aliquam velit dolore iste facere iure debitis. Sunt fugit
+              beatae hic pariatur sapiente, quas iure est esse deleniti, rerum
+              quia temporibus corporis reiciendis iusto totam explicabo
+              praesentium ab eveniet incidunt cumque? Adipisci, voluptate
+              pariatur rem aliquid culpa debitis illo corrupti, quia repellat
+              sint mollitia saepe, architecto magni dolore. Fuga, architecto
+              alias? Aut nihil explicabo sit exercitationem aliquam, impedit
+              illum ipsa nostrum quae. Doloribus recusandae, at optio provident
+              quos vitae. Ipsum temporibus optio mollitia doloremque quod, ea at
+              quasi, recusandae deleniti hic, in alias ullam! Porro accusamus
+              quasi culpa quo ab suscipit autem dolorum, nam quaerat quia labore
+              laudantium mollitia distinctio, fuga blanditiis sint? Blanditiis
+              quibusdam fugit quos adipisci animi, labore minima totam, sed
+              tempore consectetur illum officia dicta exercitationem aspernatur
+              ab omnis fuga optio consequuntur distinctio eos perspiciatis
+              placeat, nesciunt quod natus. Officiis, unde! Tenetur quod fuga,
+              ea dolores totam ad reprehenderit ex sint corporis facilis, quia,
+              rerum quae voluptate. Architecto qui nam cumque dolorem, beatae
+              explicabo neque distinctio veritatis consectetur reiciendis dolor
+              blanditiis est atque, cum natus, assumenda corporis! Vitae earum
+              dolor corrupti blanditiis eaque optio, tempore doloremque
+              exercitationem quisquam nemo rem explicabo odio commodi ex laborum
+              quasi, reprehenderit corporis, dolore rerum libero aliquid magni?
+              Cum id, quam, at expedita blanditiis enim soluta itaque fuga ad
+              repudiandae laudantium. Mollitia optio similique unde incidunt
+              atque aperiam facilis cum eaque nemo dolore pariatur sequi
+              architecto dolor impedit doloremque assumenda at tempora cumque
+              ratione eius ullam eum ipsa, hic ea! Vero tempore corporis
+              necessitatibus possimus facere, esse minima ea modi enim
+              consequuntur quis nam maiores eius, est sed aspernatur provident
+              quod ipsum, doloribus laudantium. Nulla architecto sapiente cumque
+              vero, tenetur dicta corporis placeat officiis explicabo dolorem
+              voluptatem laborum consequatur, harum saepe obcaecati nemo
+              asperiores dolores odit itaque facilis reiciendis repellendus
+              praesentium in dolor. Exercitationem, nesciunt ab. Mollitia
+              perferendis cupiditate alias ea harum esse asperiores laborum
+              dignissimos, dolorum aut totam expedita perspiciatis officiis
+              tempora veniam adipisci animi ipsa odio debitis molestias,
+              temporibus sunt minus! Eum necessitatibus officiis ab deserunt
+              natus excepturi iure. Cum quaerat voluptas accusamus quod ut
+              libero vitae totam! Perspiciatis, deleniti laborum, expedita
+              veritatis labore neque nesciunt nemo harum eos, minima nulla.
+              Provident sunt facere soluta vero porro nulla consequuntur
+              recusandae hic est molestias vel repudiandae blanditiis ea
+              delectus, quidem a laborum officia rem numquam, officiis maxime
+              impedit quas sed! Unde suscipit consectetur, accusamus, corporis
+              iure voluptatibus alias facilis minus ducimus ullam vitae atque
+              incidunt ut consequuntur quisquam rem! Doloremque, placeat. Sit
+              quam soluta sapiente esse, voluptatum temporibus aliquam veniam
+              expedita ea libero, eveniet commodi nesciunt? Dolor iure cum iusto
+              eius pariatur vitae culpa tempora delectus magnam voluptatibus
+              officiis consequuntur impedit distinctio, error enim recusandae
+              debitis quaerat a minus facere, accusamus voluptatum..
+            </p>
+          </AccordionItem>
+          <AccordionItem title="Why you are so beautiful?">
+          <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit cumque neque tempora aspernatur ratione ullam ipsum
+              vero impedit quibusdam commodi optio accusamus iusto praesentium
+              dicta ipsa blanditiis laudantium omnis illum pariatur labore,
+              repellendus provident quo sapiente inventore? Placeat sint,
+              mollitia fugit excepturi quae ad recusandae ullam quo consequuntur
+              illo quos labore sit fugiat obcaecati a provident nam repudiandae
+              maiores aliquam velit dolore iste facere iure debitis. Sunt fugit
+              beatae hic pariatur sapiente, quas iure est esse deleniti, rerum
+              quia temporibus corporis reiciendis iusto totam explicabo
+              praesentium ab eveniet incidunt cumque? Adipisci, voluptate
+              pariatur rem aliquid culpa debitis illo corrupti, quia repellat
+              sint mollitia saepe, architecto magni dolore. Fuga, architecto
+              alias? Aut nihil explicabo sit exercitationem aliquam, impedit
+              illum ipsa nostrum quae. Doloribus recusandae, at optio provident
+              quos vitae. Ipsum temporibus optio mollitia doloremque quod, ea at
+              quasi, recusandae deleniti hic, in alias ullam! Porro accusamus
+              quasi culpa quo ab suscipit autem dolorum, nam quaerat quia labore
+              laudantium mollitia distinctio, fuga blanditiis sint? Blanditiis
+              quibusdam fugit quos adipisci animi, labore minima totam, sed
+              tempore consectetur illum officia dicta exercitationem aspernatur
+              ab omnis fuga optio consequuntur distinctio eos perspiciatis
+              placeat, nesciunt quod natus. Officiis, unde! Tenetur quod fuga,
+              ea dolores totam ad reprehenderit ex sint corporis facilis, quia,
+              rerum quae voluptate. Architecto qui nam cumque dolorem, beatae
+              explicabo neque distinctio veritatis consectetur reiciendis dolor
+              blanditiis est atque, cum natus, assumenda corporis! Vitae earum
+              dolor corrupti blanditiis eaque optio, tempore doloremque
+              exercitationem quisquam nemo rem explicabo odio commodi ex laborum
+              quasi, reprehenderit corporis, dolore rerum libero aliquid magni?
+              Cum id, quam, at expedita blanditiis enim soluta itaque fuga ad
+              repudiandae laudantium. Mollitia optio similique unde incidunt
+              atque aperiam facilis cum eaque nemo dolore pariatur sequi
+              architecto dolor impedit doloremque assumenda at tempora cumque
+              ratione eius ullam eum ipsa, hic ea! Vero tempore corporis
+              necessitatibus possimus facere, esse minima ea modi enim
+              consequuntur quis nam maiores eius, est sed aspernatur provident
+              quod ipsum, doloribus laudantium. Nulla architecto sapiente cumque
+              vero, tenetur dicta corporis placeat officiis explicabo dolorem
+              voluptatem laborum consequatur, harum saepe obcaecati nemo
+              asperiores dolores odit itaque facilis reiciendis repellendus
+              praesentium in dolor. Exercitationem, nesciunt ab. Mollitia
+              perferendis cupiditate alias ea harum esse asperiores laborum
+              dignissimos, dolorum aut totam expedita perspiciatis officiis
+              tempora veniam adipisci animi ipsa odio debitis molestias,
+              temporibus sunt minus! Eum necessitatibus officiis ab deserunt
+              natus excepturi iure. Cum quaerat voluptas accusamus quod ut
+              libero vitae totam! Perspiciatis, deleniti laborum, expedita
+              veritatis labore neque nesciunt nemo harum eos, minima nulla.
+              Provident sunt facere soluta vero porro nulla consequuntur
+              recusandae hic est molestias vel repudiandae blanditiis ea
+              delectus, quidem a laborum officia rem numquam, officiis maxime
+              impedit quas sed! Unde suscipit consectetur, accusamus, corporis
+              iure voluptatibus alias facilis minus ducimus ullam vitae atque
+              incidunt ut consequuntur quisquam rem! Doloremque, placeat. Sit
+              quam soluta sapiente esse, voluptatum temporibus aliquam veniam
+              expedita ea libero, eveniet commodi nesciunt? Dolor iure cum iusto
+              eius pariatur vitae culpa tempora delectus magnam voluptatibus
+              officiis consequuntur impedit distinctio, error enim recusandae
+              debitis quaerat a minus facere, accusamus voluptatum..
+            </p>
+          </AccordionItem>
+          {/* Add more AccordionItem components as needed */}
+        </Accordion>
+        </div>
+    
+      </Modal>
     </div>
   );
 };
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,30 +1,28 @@
-// Modal.jsx
-import React from "react";
+import React, { ReactNode } from "react";
 import Button from "../Button";
-type TModal={
-      modal:boolean;
-      setModal:React.Dispatch<React.SetStateAction<boolean>>
-}
-const Modal = ({ modal, setModal }:TModal) => {
-  // Add or remove a class to trigger the CSS transition
-  document.body.classList.toggle("modal-open", modal);
-
+type TModal = {
+  modal: boolean;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+  children: ReactNode;
+};
+const Modal = ({ modal, setModal, children }: TModal) => {
   const closeModal = () => {
     setModal(false);
-    // Remove the class on modal close
     document.body.classList.remove("modal-open");
   };
 
   return (
-    <div className={`fixed inset-0 flex items-center justify-center ${modal ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
-      <div className={`relative bg-slate-400 p-6 rounded-lg`}>
-        <h1>Hey! it works now</h1>
-        <div className={`absolute top-1 right-1`}>
-          <Button
-            onClick={closeModal}
-            title="X"
-            className="btn-warning"
-          >
+    <div
+      className={`fixed inset-0 flex items-center justify-center overflow-y-auto ${
+        modal ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <div className="relative bg-slate-400 p-6 rounded-lg max-w-[80%] max-h-[80%] overflow-y-auto">
+        <div className="max-h-full overflow-y-auto">
+          {children}
+        </div>
+        <div className="absolute top-1 right-1">
+          <Button onClick={closeModal} title="X" className="btn-warning">
             Close Modal
           </Button>
         </div>
